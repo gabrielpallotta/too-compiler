fc2e0cbbd6a8c82bbe2655023d7851cf7c6c8806
Gabriel Pallotta <gabriel.cardenete@hotmail.com.com>
2017-11-27 09:09:35 -0200

    Consertei analisador léxico

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <stdio.h>
+#include "header/AnalisadorLexico.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+
+    AnalisadorLexico* analex = new AnalisadorLexico((char*)"teste.pas");
+
+    TipoPedaco tipo;
+    while (analex->temMaisPedacos()) {
+      tipo = analex->proximoPedaco();
+      if (tipo < nPalavras + nOperadores)
+        printf("%s\n", palavrasReservadas[tipo]);
+      else
+      {
+        if (tipo == numero)
+          printf("Número\n");
+        if (tipo == identificador)
+          printf("Identificador\n");
+      }
+
+    }
     return 0;
 }
